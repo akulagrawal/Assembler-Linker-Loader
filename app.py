aae4c419d53c91d68fbe7164549fa66867f8ae5f
Akul Agrawal <akulagrawal@rediffmail.com>
2018-04-04 20:27:53 +0530

    Authors added

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def runSimulator():
 		return json.dumps({'status':'OK', 'reg':reg, 'memory':memory, 'memoryData':memoryData, 'stack':stack})
 
 if __name__=="__main__":
-	app.run(host='0.0.0.0', debug=True)
+	app.run(host='127.0.0.1', debug=True)
